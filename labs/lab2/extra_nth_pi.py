147ca8ee62d1b66e8ebb584e6ddb4a75c0b200fc
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-02-08 23:29:04 -0700

    Corrected and added test for lab 2 extra credit

---
+++ labs/lab2/extra_nth_pi.py
@@ -0,0 +1,27 @@
+def expmod(base, power, mod_by):
+    """This function calculates (base ^ power) % mod_by."""
+
+    x = 1
+    while(power > 0):
+        if (power & 1 == 1):
+            x = (x * base) % mod_by
+        base = (base * base) % mod_by
+        power >>= 1
+    return x % mod_by
+
+
+def decimal_to_hex(frac, ndigits):
+    """Convert the float frac into a hexadecimal number of
+    length ndigits."""
+
+    frac = abs(frac)
+    s = []
+    for i in range(ndigits):
+        frac = 16 * (frac - int(frac))
+        s.append("0123456789ABCDEF"[int(frac)])
+    return ''.join(s)
+
+
+def digits(n):
+    nth_digit = 0.141592653589
+    return decimal_to_hex(nth_digit, 16)
