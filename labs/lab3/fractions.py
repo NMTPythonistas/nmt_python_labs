25ced0ef72ce949b8f77e91225db3ea3e7d06775
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-02-16 22:45:11 -0700

    Template for fractions.

---
+++ labs/lab3/fractions.py
@@ -0,0 +1,23 @@
+def multiply(fraction1, fraction2):
+    """Takes two fractions f1 and f2, returns f1 * f2."""
+    return (0, 0) # very wrong
+
+
+def divide(fraction1, fraction2):
+    """Takes two fractions f1 and f2, returns f1 / f2."""
+    return (0, 0) # very very wrong
+
+
+def find_minimum(fractions_list):
+    """Find the smallest fractions in a list of fractions.
+
+    fractions_list - non-empty list of fractions."""
+    return fractions_list[0] # this is very wrong
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
