b9c7ccbf4fab0a8d759e839ab64078103b36773b
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-03-02 17:23:44 -0700

    Renamed lab3.5 -> lab4, incremented lab numbers.

--- labs/lab3.5/all_different.py
+++
@@ -1,6 +0,0 @@
-def check(elements):
-    """This function takes a list and returns True if all of its elements
-    are different or if the list is empty. It returns False otherwise.
-
-    This is an incomplete definition."""
-    return False
