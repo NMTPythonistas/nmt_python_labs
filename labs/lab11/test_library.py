bcaaaeaba23852ef1c243f959d170e02430c75ee
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-03-22 21:38:18 -0600

    Renamed lab 10 supplementary files.

--- labs/lab11/test_library.py
+++ labs/lab11/test_library.py
@@ -1,11 +1,11 @@
 import unittest
 
 class BookTest(unittest.TestCase):
-    def create_and_str_book(self):
+    def test_initialization(self):
         from library import Book
         tests = [('The Life and Lies of Albus Dumbledore', 'Rita Skeeter'),
-            ('Some Other Book', 'Some Person'),
-            ('Lab 9', 'Russell White')]
+                 ('Some Other Book', 'Some Person'),
+                 ('Lab 9', 'Russell White')]
         for title, author in tests:
             book = Book(title, author)
             self.assertIn(title, str(book))
@@ -15,9 +15,9 @@ class LibraryTest(unittest.TestCase):
     def test_get_authors(self):
         from library import Book, Library
         tests = [Book('The Life and Lies of Albus Dumbledore', 'Rita Skeeter'),
-            ('Some Other Book by Someone'),
-            Book('Lab 9', 'The Best TA'),
-            Book('Yet Another Book', 'Someone')]
+                 ('Some Other Book by Someone'),
+                 Book('Lab 9', 'The Best TA'),
+                 Book('Yet Another Book', 'Someone')]
         lib = Library()
         for test in tests:
             lib.add_book(test)
@@ -25,9 +25,5 @@ class LibraryTest(unittest.TestCase):
         self.assertIn('Someone', lib.get_authors())
         self.assertNotIn('', lib.get_authors())
 
-
-
-
-
-class TreeTest(unittest.TestCase):
-    pass
+if __name__ == "__main__":
+    unittest.main()
